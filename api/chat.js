b3ac54770bd01759ea6b51f814f5d2d00d7a521d
Aadarsh-1000 <aadarsh.k.230069@standrewsindia.com>
2026-05-23 19:46:37 +0530

    i am chaing model form gemini to groq

--- api/chat.js
+++ api/chat.js
@@ -5,23 +5,23 @@ export default async function handler(req, res){
   try{
 
   const ai = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
-      {
+"https://api.groq.com/openai/v1/chat/completions",
+  {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          contents: [
-            {
-              parts: [
-                {
-                  text: message
-                }
-              ]
-            }
-          ]
-        })
+      headers: {
+  "Content-Type": "application/json",
+  "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
+},
+       body: JSON.stringify({
+  model: "llama-3.3-70b-versatile",
+
+  messages: [
+    {
+      role: "user",
+      content: message
+    }
+    ]
+} )
       }
       
     );
@@ -29,7 +29,8 @@ export default async function handler(req, res){
     const data =await ai.json();
     console.log(data);
       res.json({
-reply: data?.candidates?.[0]?.content?.parts?.[0]?.text || JSON.stringify(data)
+reply:
+data?.choices?.[0]?.message?.content
   });
 }
 catch(error) {
