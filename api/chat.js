78969caa2d47be6a3efd4ead56b07cb5e6d22dea
Aadarsh-1000 <aadarsh.k.230069@standrewsindia.com>
2026-05-23 18:06:56 +0530

    chnaged chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,45 +1,32 @@
 export default async function handler(req, res) {
 
-  try {
+  const { message } = req.body;
 
-    const { message } = req.body;
+  const ai = await fetch(
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        contents: [
+          {
+            parts: [
+              {
+                text: message
+              }
+            ]
+          }
+        ]
+      })
+    }
+  );
 
-    const ai = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          contents: [
-            {
-              parts: [
-                {
-                  text: message
-                }
-              ]
-            }
-          ]
-        })
-      }
-    );
+  const data = await ai.json();
 
-    const data = await ai.json();
-    console.log(data);
+  console.log(data);
 
-    res.json({
-    reply:
-  data.candidates[0].content.parts[0].text
-        || "No response"
-    });
-
-  } catch (error) {
-
-    res.json({
-      reply: error.message
-    });
-
-  }
+  res.json(data);
 
 }
