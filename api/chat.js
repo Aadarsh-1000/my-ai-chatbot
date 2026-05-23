b1129daac10e8c2194ad9d76ec084a40b528a355
Aadarsh-1000 <aadarsh.k.230069@standrewsindia.com>
2026-05-23 19:30:53 +0530

    i added a colnsole.log to show what the rror is

--- api/chat.js
+++ api/chat.js
@@ -32,6 +32,7 @@ export default async function handler(req, res){
  catch(error) {
 
    console.log(error);
+   console.log(error.message);
 
    res.status(500).json({
       reply: "Backend crashed"
