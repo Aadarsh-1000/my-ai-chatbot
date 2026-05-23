4bc02498de4a17060705d78ba68e67e7ca309d73
Aadarsh-1000 <aadarsh.k.230069@standrewsindia.com>
2026-05-23 18:01:53 +0530

    changed my chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,9 @@
-export default async function handler(req, res){
+export default async function handler(req, res) {
+
+  try {
+
+    const { message } = req.body;
+
     const ai = await fetch(
       `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
@@ -19,9 +24,21 @@ export default async function handler(req, res){
         })
       }
     );
-    const data =await ai.json();
-      res.json({
-    reply: data.candidates[0].content.parts[0].text
-  });
+
+    const data = await ai.json();
+
+    res.json({
+      reply:
+        data.candidates?.[0]?.content?.parts?.[0]?.text
+        || "No response"
+    });
+
+  } catch (error) {
+
+    res.json({
+      reply: error.message
+    });
+
+  }
 
 }
