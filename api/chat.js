ad8c382a6dca620c6fc7c900369075c919fff789
Aadarsh-1000 <aadarsh.k.230069@standrewsindia.com>
2026-05-26 17:05:04 +0530

    got back original code

--- api/chat.js
+++ api/chat.js
@@ -1,63 +1,44 @@
 import "dotenv/config";
+import "dotenv/config";
+export default async function handler(req, res){
+  const {message} =  req.body 
+  try{
 
-import { ChatGroq } from "@langchain/groq";
+  const ai = await fetch(
+"https://api.groq.com/openai/v1/chat/completions",
+  {
+        method: "POST",
+      headers: {
+  "Content-Type": "application/json",
+  "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
+},
+       body: JSON.stringify({
+  model: "llama-3.3-70b-versatile",
 
-import {
-  HumanMessage,
-  AIMessage
-} from "@langchain/core/messages";
-
-const memoryStore = {};
-
-export default async function handler(req, res) {
-
-  try {
-
-    const { message, userId } = req.body;
-
-    // create user memory
-    if (!memoryStore[userId]) {
-      memoryStore[userId] = [];
+  messages: [
+    {
+      role: "user",
+      content: message
     }
-
-    // get conversation history
-    const history = memoryStore[userId];
-
-    // add user message
-    history.push(
-      new HumanMessage(message)
+    ]
+} )
+      }
+      
     );
-
-    // create model
-    const model = new ChatGroq({
-      apiKey: process.env.GROQ_API_KEY,
-      model: "llama-3.3-70b-versatile",
-      temperature: 0.1
-    });
-
-    // invoke model with history
-    const result = await model.invoke(history);
-
-    // save ai response
-    history.push(
-      new AIMessage(result.content)
-    );
-
-    // send response
-    res.status(200).json({
-      reply: result.content
-    });
-
-  }
-
-  catch (error) {
-
-    console.log("FULL ERROR:", error);
-
-    res.status(500).json({
-      reply: "Something went wrong"
-    });
-
-  }
-
+   
+    const data =await ai.json();
+    console.log(data);
+      res.json({
+reply:
+data?.choices?.[0]?.message?.content
+  });
 }
+catch(error) {
+
+   console.log("FULL ERROR:");
+
+
+   
+  }
+}
+  
