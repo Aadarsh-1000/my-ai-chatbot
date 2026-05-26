2d4f9016f1bf84f90796dd445c315a573f211968
Aadarsh-1000 <aadarsh.k.230069@standrewsindia.com>
2026-05-26 08:13:52 +0530

    amde it ot not take gemil automatically

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ window.onload = function () {
      }
     google.accounts.id.initialize({
         client_id: "1066541156045-her69mn6bh1av8eo2rin0n2o6n0t5c73.apps.googleusercontent.com",
-        callback: handleCredentialResponse
+        callback: handleCredentialResponse,
+        auto_select= false
     });
 
    google.accounts.id.renderButton(
