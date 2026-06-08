b0d3eaea2de252f4b734152141dd0b36b3f00ed6
Aadarsh-1000 <aadarsh.k.230069@standrewsindia.com>
2026-06-08 22:57:24 +0530

    added a mouse aloower

--- script.js
+++ script.js
@@ -265,3 +265,23 @@ mic.onclick = () => {
     };
 
 };
+const circleElement = document.querySelector('.circle');
+const mouse = {x: 0, y: 0},
+    circle = {x:0, y:0};
+
+windows.addEventListener('mousemove', e => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+});
+
+const speed = 0.15;
+
+const tick = () => {
+    circle.x += (mouse.x - circle.x) * speed;
+    circle.y += (mouse.y - circle.y) * speed;
+
+ circleElement.style.transform = `translate($(circle.x)px, ${circle.y}px)`;
+ window.requestAnimationFrame(tick);
+}
+
+tick();
